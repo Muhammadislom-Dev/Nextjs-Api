0a0d7d771b69025c0173eaae895ebe3ac36503ee
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-17 01:02:02 +0500

    add

---
+++ pages/product/index.js
@@ -0,0 +1,11 @@
+function ProductList(){
+    return(
+        <div>
+            <h2>Product 1</h2>
+            <h2>Product 2</h2>
+            <h2>Product 3</h2>
+        </div>
+    )
+}
+
+export default ProductList
