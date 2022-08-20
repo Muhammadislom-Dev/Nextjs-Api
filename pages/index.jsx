6215e6bc4051d74ec8429cc643620d17809bbc62
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-20 18:03:44 +0500

    add

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,38 @@
-function Home(){
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+function Home() {
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    axios
+      .get(`https://api.oxo.uz/api/v1/forms/get-form/`)
+      .then(res => {
+        setData(res.data)
+        setIsLoading(false)
+        console.log(data);
+      })
+      .catch(error => {
+        setError(error.message)
+        setIsLoading(false)
+      })
+  }, [])
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  if (error) {
+    return <h2>{error}</h2>
+  }
+
+
   return(
     <div>
-        <h1>Hello world</h1>
+        Hello world
     </div>
   )
 }
