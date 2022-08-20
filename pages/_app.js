f61d18b460b58543a7ec1010fd846e191e52433c
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-08-20 18:04:04 +0500

    add

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import "../styles/globals.css";
-import Script from "next/script";
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script 
-      strategy="lazyOnload"
-      src={`https://www.googletagmanager.com/gtag/js?id=`}
-      />
       <Component {...pageProps} />
     </>
   );
